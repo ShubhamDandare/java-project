ce3355fb4ce66e957dbaf020c5311188c460db02
Pratik Dandare <pratik.dandare@outlook.com>
2020-02-11 15:19:59 +0530

    arrye file add

---
+++ 5th day/Array.java
@@ -0,0 +1,14 @@
+public class Array{
+
+public static void main(String args[]){
+
+int a[]=new int[5];
+a[0]=10;
+a[1]=20;
+a[4]=40;
+System.out.println(a[0]);
+System.out.println(a[1]);
+System.out.println(a[2]);
+System.out.println(a[04]);
+}
+}
---
+++ 5th day/Array1.java
@@ -0,0 +1,12 @@
+public class Array1{
+
+public static void main(String args[]){
+
+int a[]=new int[10];
+for(int i=0;i<10;i++){
+
+a[i]=i+1;
+System.out.println(a[i]);
+}
+}
+}
